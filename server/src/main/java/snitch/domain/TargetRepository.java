13e62f249d116e717daa9cd3adddd3f25608fc83
Роман Суслин <rs7@mail.ru>
2016-06-10 17:28:36 +0600

    Поиск Объекта слежки по vk-идентификатору

--- server/src/main/java/snitch/domain/TargetRepository.java
+++ server/src/main/java/snitch/domain/TargetRepository.java
@@ -1,6 +1,12 @@
 package snitch.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 public interface TargetRepository extends CrudRepository<Target, Integer> {
+    @RestResource(path = "vk")
+    List<Target> findByVkID(@Param("id") Integer vkID);
 }
