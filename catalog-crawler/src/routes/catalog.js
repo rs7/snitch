09751a3fb3e4d91558146d95aa8ad953bc54eb32
Роман Суслин <rs7@mail.ru>
2016-06-14 17:28:50 +0600

    Добавлен метод получения vk-идентификаторов по сотне

--- catalog-crawler/src/routes/catalog.js
+++ catalog-crawler/src/routes/catalog.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var getHundred = require('./../controller/getHundred');
 var getLast = require('./../controller/getLast');
 var router = express.Router();
 
 router.get('/last', function (req, res, next) {
     getLast(res.callback);
+});
+
+router.get('/hundred/:id', function (req, res, next) {
+    var hundred = req.params.id;
+    getHundred(hundred, res.callback);
 });
 
 module.exports = router;
